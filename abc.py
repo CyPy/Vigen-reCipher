afcce091fdff74fef172cb38d53929fb591861fd
priyanklodha <priyankpkl@hotmail.com>
2015-09-03 01:57:47 +0530

    encode and decode for the cipher

---
+++ abc.py
@@ -0,0 +1,57 @@
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+
+def encode(plaintext, key):
+	"""
+	Encodes the plaintext using the key provided and 
+	returns the ciphertext.
+	"""
+	plaintext = plaintext.lower()
+	key = key.lower()
+	for i in key:
+		if (ord(i)<97 or ord(i)>122) :
+			return "Invalid Key: Key can only have alphabets i.e. no special characters or spaces."
+	ciphertext = ""
+	length = len(key)
+	x=0
+	for i in plaintext:
+		if (alphabets.find(i) != -1):
+			finalIndex = (alphabets.find(i) + ord(key[x]) - 97) % 26
+			ciphertext += alphabets[finalIndex]
+			if x == length - 1 :
+				x = 0
+			else:
+				x = x + 1
+		else:
+			ciphertext += i
+	return ciphertext
+
+
+
+def decode(ciphertext, key):
+	"""
+	Decodes the ciphertext using the key provided and
+	returns the plaintext.
+	"""
+	ciphertext = ciphertext.lower()
+	key = key.lower()
+	for i in key:
+		if (ord(i)<97 or ord(i)>122) :
+			return "Invalid Key: Key can only have alphabets i.e. no special characters or spaces."			
+	plaintext = ""
+	length = len(key)
+	x=0
+	for i in ciphertext:
+		if (alphabets.find(i) != -1):
+			if ((alphabets.find(i) - ord(key[x]) + 97) > 0):
+				finalIndex = (alphabets.find(i) - ord(key[x]) + 97)
+			else:
+				finalIndex = (alphabets.find(i) - ord(key[x]) + 97 + 26) % 26
+			plaintext += alphabets[finalIndex]
+			if x == length - 1 :
+				x = 0
+			else : 
+				x = x + 1 
+		else:
+			plaintext += i
+	return plaintext
+
